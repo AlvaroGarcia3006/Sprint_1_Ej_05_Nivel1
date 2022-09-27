69e3d5b1a53ae5633398811992d6b5a435b32ba3
AlvaroGarcia3006 <113432141+AlvaroGarcia3006@users.noreply.github.com>
2022-09-27 17:48:43 +0200

    Listar Recursivo

---
+++ src/main/java/nivel1_ej02/Main.java
@@ -0,0 +1,29 @@
+package nivel1_ej02;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        File dir = new File("C:\\Users\\alvar\\IdeaProjects\\Sp1_ej05_GarciaAlvaro\\src\\main\\java");
+        File[] list = dir.listFiles();
+        Arrays.sort(list);
+        for (File l : list){
+            System.out.println(l.getName());
+            DirList(l);
+            System.out.println();
+        }
+    }
+    public static void DirList(File files){
+       File[] list = files.listFiles();
+       Arrays.sort(list);
+       for (File f : list) {
+            if (f.isDirectory()){
+                System.out.println("\t"+f.getName()+ " (D)");
+                DirList(f);
+            }else{
+                System.out.println("\t"+f.getName()+" (F)");
+            }
+       }
+    }
+}
